968d05ab635b8a1d8d9f7bb4e4b389bd3243e7a0
Momin Hosan Emon <mominhasanemon@gmail.com>
2026-03-03 15:22:04 +0600

    course fetch api added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3819073106" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3272680616" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
